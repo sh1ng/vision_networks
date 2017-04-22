d4ba660b1ceb26812fad4a60e1311ae16fb50f50
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2017-04-22 23:36:26 +0200

    no message

--- run_smooth_net.py
+++ run_smooth_net.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         default='SmoothNet',
         help='What type of model to use')
     parser.add_argument(
-        '--growth_rate', '-k', type=int, choices=[24, 36, 48, 60, 72],
+        '--growth_rate', '-k', type=int,
         default=24,
         help='Grows rate for every layer, '
              'choices were restricted to used in paper')
